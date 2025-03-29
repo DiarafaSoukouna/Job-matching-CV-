37049ee0176d635234f3f098143d2b7fd35ed6f9
Tiemoko <tiemokosogodogo97@gmail.com>
2025-03-29 17:12:32 +0000

    Start impl ...

--- main.py
+++ main.py
@@ -1,28 +1,69 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
-from models import Item
 
-app = FastAPI()
+from models import Account
 
-@app.post("/item/create")
-async def create_item(item: Item):
-    return item
+from typing import Annotated
 
-# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Session, SQLModel, create_engine, select
 
+database_url = "postgresql://test_db_user:kZvMBdDnQxLxOHSjtGztxEap2GHQ8tWJ@dpg-cviqgei4d50c73c4t2ag-a.virginia-postgres.render.com/test_db_tzs3"
 
-# @app.get("/items/")
-# async def read_item(skip: int = 0, limit: int = 10):
-#     return fake_items_db[skip : skip + limit]
+engine = create_engine(database_url)
+
+def drop_create_db_and_tables():
+    ###Drop all tables
+    SQLModel.metadata.drop_all(engine)
+
+    ###Drop all tables
+    SQLModel.metadata.create_all(engine)
+
+    ddd = Account()
+    
+    # with Session(engine) as session:
+    #     product1 = Product(name="Laptop", price=1200.0)
+    #     service1 = Service(description="Web Development", duration=40)
+
+    #     session.add(product1)
+    #     session.add(service1)
+    #     session.commit()
+
+    #     products = session.query(Product).all()
+    #     services = session.query(Service).all()
+
+    #     print("Products:", products)
+    #     print("Services:", services)
+
+    #     items = session.query(BaseItem).all()
+    #     for item in items:
+    #         if isinstance(item, Product):
+    #             print(f"Found Product: {item.name}, Price: {item.price}")
+    #         elif isinstance(item, Service):
+    #             print(f"Found Service: {item.description}, Duration: {item.duration}")
+    #         else:
+    #             print(f"Found BaseItem: {item.type}")
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+###On startup/shutdow actions
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    ###Actions to on startup
+    drop_create_db_and_tables()
+    yield
+
+    ###Actions to on shutdown
+    #Code...
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-@app.get("/users")
-async def read_users():
-    return ["Rick", "Morty"]
+async def home():
+    return "Hello world, ..."
--- models.py
+++ models.py
@@ -1,7 +1,71 @@
-from pydantic import BaseModel
+from sqlmodel import Field, Relationship, SQLModel
+from typing import ClassVar
+from enum import Enum
+from datetime import datetime
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+class AccountType(str, Enum):
+    ADMIN = "ADMIN"
+    CANDIDATE = "CANDIDATE"
+    ENTREPRISE = "ENTREPRISE"
+
+class AccountBase(SQLModel, table=False):
+    id: int = Field(primary_key=True, nullable=False)
+
+    account_type: AccountType = Field(nullable=False)
+
+    enabled: bool = Field(nullable=False, default=False)
+
+    deleted: bool = Field(nullable=False, default=False)
+
+    full_name: str = Field(index=True)
+
+    username: str | None = Field(unique=True, nullable=False, index=True)
+
+    email: float = Field(unique=True, nullable=False, index=True)
+
+    phone_number: str | None = Field(unique=True, default=None, index=True)
+
+    address: str | None = Field(default=None)
+
+    create_date: ClassVar[datetime] = Field(nullable=False, default=datetime.now())
+
+
+class Account(AccountBase, table=True):
+    __tablename__="account"
+    password: str | None = Field(default=None)
+    cv_file_path: str | None = Field(default=None)
+    nif: str | None = Field(default=None)
+    rc: str | None = Field(default=None)
+
+    jobs: list["Job"] | None = Relationship(back_populates="account")
+
+class Admin(AccountBase, table=False):
+    pass
+
+class Entreprise(AccountBase, table=False):
+    nif: str | None = None
+    rc: str | None = None
+    jobs: list["Job"] | None = None
+
+class Candidate(AccountBase, table=False):
+    cv_file_path: str | None = None
+
+class JobCategory(SQLModel, table=True):
+    __tablename__="job_category"
+    id: int = Field(primary_key=True, nullable=False)
+    name: str = Field(nullable=False, unique=True)
+    description: str | None = Field(default=None)
+
+    jobs: list["Job"] = Relationship(back_populates="category")
+
+class Job(SQLModel, table=True):
+    __tablename__="job"
+    id: int = Field(primary_key=True, nullable=False)
+    title: str = Field(nullable=False)
+    description: str | None = Field(nullable=False)
+
+    category_id: int | None = Field(default=None, foreign_key="job_category.id")
+    category: JobCategory | None = Relationship(back_populates="jobs")
+
+    account_id: int | None = Field(default=None, foreign_key="account.id")
+    account: Account | None = Relationship(back_populates="jobs")
